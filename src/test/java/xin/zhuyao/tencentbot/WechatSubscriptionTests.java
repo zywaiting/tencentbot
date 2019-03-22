6a32aad44d2c7283f82cad57e52e00103d85cd31
zy <1573240324@qq.com>
2019-03-22 18:55:30 +0800

    修改一小部分

--- src/test/java/xin/zhuyao/tencentbot/WechatSubscriptionTests.java
+++ src/test/java/xin/zhuyao/tencentbot/WechatSubscriptionTests.java
@@ -4,7 +4,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.internal.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
